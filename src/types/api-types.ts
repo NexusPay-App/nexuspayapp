517829f76d7a319a6e86ee4ef61484728203d9b2
anthonykimani <kimaniantoni@gmail.com>
2024-04-20 14:15:57 +0300

    fix: fixes client application error fix: sets empty transaction state

--- src/types/api-types.ts
+++ src/types/api-types.ts
@@ -1,23 +1,21 @@
-
-export type Transactions = {
-    blockHash: string;
-    blockNumber: string;
-    confirmations: string;
-    contractAddress: string;
-    cumulativeGasUsed: string;
-    from: string;
-    gas: string;
-    gasPrice: string;
-    gasUsed: string;
-    hash: string;
-    input: string;
-    nonce: string;
-    timeStamp: string;
-    to: string;
-    tokenDecimal: string;
-    tokenName: string;
-    tokenSymbol: string;
-    transactionIndex: string;
-    value: string;
-  };
-  
+export type TransactionsType = {
+  blockHash: string;
+  blockNumber: string;
+  confirmations: string;
+  contractAddress: string;
+  cumulativeGasUsed: string;
+  from: string;
+  gas: string;
+  gasPrice: string;
+  gasUsed: string;
+  hash: string;
+  input: string;
+  nonce: string;
+  timeStamp: string;
+  to: string;
+  tokenDecimal: string;
+  tokenName: string;
+  tokenSymbol: string;
+  transactionIndex: string;
+  value: string;
+};

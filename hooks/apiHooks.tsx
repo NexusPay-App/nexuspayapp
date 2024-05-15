d80812afacc60278886e478af281b28e3bcc19f8
doyeninnit <agatenashons@gmail.com>
2024-05-15 13:43:02 +0300

    fix deployment errorss

--- hooks/apiHooks.tsx
+++ hooks/apiHooks.tsx
@@ -1,9 +1,59 @@
+// import { useQuery } from "@tanstack/react-query";
+// import useAxios from "./useAxios";
+// import { Transaction } from "@/types/api-types";
+
+// interface ApiHookResponse<T> {
+//   data: T;
+//   isLoading: boolean;
+//   error: unknown;
+// }
+
+// export function useGetConversionRate(): ApiHookResponse<number> {
+//   const api = useAxios();
+//   const { isLoading, data, error } = useQuery({
+//     queryKey: ["getConversionRate"],
+//     queryFn: () =>
+//       api.get("usdc/conversionrate").then((res) => {
+//         // console.log(res);
+//         return res.data.rate;
+//       }),
+//   });
+//   return { isLoading, data, error };
+// }
+
+// export function useGetBalanceHook(): ApiHookResponse<number> {
+//   const api = useAxios();
+//   const user = JSON.parse(localStorage.getItem("user") ?? "");
+//   const { isLoading, data, error } = useQuery({
+//     queryKey: ["getUserBalance"],
+//     queryFn: () =>
+//       api.get(`usdc/usdc-balance/${user.walletAddress}`).then((res) => {
+//         return res.data;
+//       }),
+//   });
+//   return { isLoading, data, error };
+// }
+
+// export function useGetTransactions(): ApiHookResponse<Transaction[]> {
+//   const api = useAxios();
+//   const user = JSON.parse(localStorage.getItem("user"));
+//   const { isLoading, data, error } = useQuery({
+//     queryKey: ["getUserTransactions"],
+//     queryFn: () =>
+//       api.get(`token/token-transfer-events?address=${user.data.walletAddress}`).then((res) => {
+//         return res.data;
+//       }),
+//   });
+//   return { isLoading, data, error };
+// }
+
+
 import { useQuery } from "@tanstack/react-query";
 import useAxios from "./useAxios";
 import { Transaction } from "@/types/api-types";
 
 interface ApiHookResponse<T> {
-  data: T;
+  data: T | undefined;
   isLoading: boolean;
   error: unknown;
 }
@@ -14,7 +64,6 @@ export function useGetConversionRate(): ApiHookResponse<number> {
     queryKey: ["getConversionRate"],
     queryFn: () =>
       api.get("usdc/conversionrate").then((res) => {
-        // console.log(res);
         return res.data.rate;
       }),
   });
@@ -23,26 +72,32 @@ export function useGetConversionRate(): ApiHookResponse<number> {
 
 export function useGetBalanceHook(): ApiHookResponse<number> {
   const api = useAxios();
-  const user = JSON.parse(localStorage.getItem("user") ?? "");
+  const userString = localStorage.getItem("user");
+  const user = userString ? JSON.parse(userString) : null;
   const { isLoading, data, error } = useQuery({
     queryKey: ["getUserBalance"],
     queryFn: () =>
-      api.get(`usdc/usdc-balance/${user.walletAddress}`).then((res) => {
+      api.get(`usdc/usdc-balance/${user?.walletAddress}`).then((res) => {
         return res.data;
       }),
+    enabled: !!user, // Only run query if user is not null
   });
   return { isLoading, data, error };
 }
 
 export function useGetTransactions(): ApiHookResponse<Transaction[]> {
   const api = useAxios();
-  const user = JSON.parse(localStorage.getItem("user"));
+  const userString = localStorage.getItem("user");
+  const user = userString ? JSON.parse(userString) : null;
   const { isLoading, data, error } = useQuery({
     queryKey: ["getUserTransactions"],
     queryFn: () =>
-      api.get(`token/token-transfer-events?address=${user.data.walletAddress}`).then((res) => {
-        return res.data;
-      }),
+      api
+        .get(`token/token-transfer-events?address=${user?.data.walletAddress}`)
+        .then((res) => {
+          return res.data;
+        }),
+    enabled: !!user, // Only run query if user is not null
   });
   return { isLoading, data, error };
 }
